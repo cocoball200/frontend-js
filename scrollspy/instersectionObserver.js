d8e55dac1bdc5ecfa75ecc40fb1d5edc35600914
mia <cocoball200@gmail.com>
2021-04-05 21:17:20 +0900

    Infinite scroll 구현을 위한 초기셋팅

--- scrollspy/instersectionObserver.js
+++ scrollspy/instersectionObserver.js
@@ -7,7 +7,7 @@ const scrollSpyObserver = new IntersectionObserver(
   (entries) => {
     const { target } = entries.find((entry) => entry.isIntersecting) || {};
     //비구조 할당을 이런식으로도 할 수 있음 ,
-    //IntersectionObserverEntry에는 isIntersecting, target등 여러가지의 속성이 있는데, isIntersection이 true인 entry을 찾고, 여기서 target값을 가져오는 것!
+    //IntersectionObserverEntry에는 isIntersecting, target등 여러가지의 속성이 있는데, isIntersection이 true인 entry을 찾고, 여기서 enry.target값을 가져오는 것!
     // false이면 {}반환 target은 해당 div를 가지고 있음. 그래서 target의 index를 가져오는 것
     const targetIndex = contentItems.indexOf(target);
     Array.from(navElem.children).forEach((c, i) => {
